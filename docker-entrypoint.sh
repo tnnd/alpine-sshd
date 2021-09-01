be5aba97771ef03f9ce43efdf2dfad279216958f
tnnd <37441897+tnnd@users.noreply.github.com>
2021-09-01 10:46:42 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,11 +2,7 @@
 
 if [ ! -f "/etc/ssh/ssh_host_rsa_key" ]; then
 	# generate fresh rsa key
-	ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
-fi
-if [ ! -f "/etc/ssh/ssh_host_dsa_key" ]; then
-	# generate fresh dsa key
-	ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
+	ssh-keygen -A
 fi
 
 #prepare run dir
